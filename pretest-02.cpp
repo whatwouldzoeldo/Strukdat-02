5039e268ae4299cca622389bdb4249344c8be672
zoel999 <48010690+zoel999@users.noreply.github.com>
2019-03-06 10:37:30 +0700

    Add files via upload

---
+++ pretest-02.cpp
@@ -0,0 +1,28 @@
+/*
+Nama        : Muhammad Zulfikar Ali
+NPM         : 140810180064
+Kelas       : B
+Deskripsi   : Move Zero to Front
+Tanggal     : 6 Maret 2019
+*/
+
+#include <iostream>
+using namespace std;
+
+void moveZeroToFront (int arr[], int n) {
+
+}
+void input (int (&arr)[100], int& n) {
+
+}
+void output (int arr[], int n) {
+
+}
+
+int main () {
+    int arr[100];
+    int n;
+    input (arr,n);
+    moveZeroToFront(arr,n);
+    output(arr,n);
+}
